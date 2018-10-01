9df3d7c0972f7ccc21271ee70a247275f43e2613
Albert Tran <trana6141@gmail.com>
2018-10-01 19:13:49 -0400

    that newnew

--- music.js
+++ music.js
@@ -10,7 +10,7 @@
 $(".submit").on("click", function (event) {
 
     event.preventDefault();
-    
+
     musicDex();
     $("#youtube").empty();
     $("#genius").empty();
@@ -22,16 +22,14 @@ function musicDex() {
 
     var searchStuff = $("#search").val().trim();
 
-    
     $.ajax({
-        url: "https://www.googleapis.com/youtube/v3/search?part=snippet&q=" 
-        + searchStuff + "&key=AIzaSyDTmuq2U1iwoNN7IDwnuJdPTClXjSUQc-o",
+        url: "https://www.googleapis.com/youtube/v3/search?part=snippet&q="
+            + searchStuff + "&key=AIzaSyDTmuq2U1iwoNN7IDwnuJdPTClXjSUQc-o",
         method: 'GET'
     }).then(function (response) {
         console.log(response.items)
         for (i = 0; i < 5; i++) {
             if (response.items[i].id.kind === "youtube#channel") {
-
                 continue
             }
 
@@ -41,42 +39,64 @@ function musicDex() {
             $("#youtube").append('<iframe width="560" height="315" src="https://www.youtube.com/embed/' + vidId + '" frameborder="100" allow="autoplay; encrypted-media" allowfullscreen></iframe>')
         }
     })
-    // var api1 = ""
-    // api1 += '?' + $.param({
 
-    // })
+    $.ajax({
+        url: "https://api.genius.com/search?q=" + searchStuff +
+            "&access_token=A3QFPk4RZGxV8fgT_41uiJiCAeyXq-UhJ-xnxipOkgZSHnShtSRVdesaqTR8axQS",
+        method: 'GET'
+    }).then(function (response) {
 
-    // var api2 = ""
-    // api2 += '?' + $.param({
+        // console.log(response);
 
-    // })
+        var artist = response.response.hits[0].result.primary_artist.api_path
 
-    // var api3 = ""
-    // api3 += '?' + $.param({
-
-    // })
-
+        // console.log(artist);
 
         $.ajax({
-            url: "https://api.genius.com/search?q=" + searchStuff + "&access_token=A3QFPk4RZGxV8fgT_41uiJiCAeyXq-UhJ-xnxipOkgZSHnShtSRVdesaqTR8axQS",
+            url: "https://api.genius.com" + artist +
+                "/?access_token=A3QFPk4RZGxV8fgT_41uiJiCAeyXq-UhJ-xnxipOkgZSHnShtSRVdesaqTR8axQS" +
+                "&text_format=html",
+            method: 'GET'
+        }).then(function (response) {
+            var bioPic = $("<div>");
+            var img = $("<img>");
+            var x = response.response;
+            img.attr("src", x.artist.image_url);
+            bioPic.append(img);
+            $("#genius").prepend(bioPic);
+            // console.log(x.artist.name);
+            // console.log(x.artist.alternate_names);
+            var name = x.artist.alternate_names;
+            var moreName = $("<div>");
+            moreName.append(name);
+            $("#genius").append(moreName)
+
+            var describe = x.artist.description.html;
+            // console.log(describe)
+            var bio = $("<div>");
+            bio.append(describe);
+            $("#genius").append(bio);
+
+        })
+
+    })
+
+
+
+
+
+
+    function topTrack() {
+        $.ajax({
+            url: "http://ws.audioscrobbler.com/2.0/?method=chart.gettoptracks&api_key=4eb509c03c98813c8a254fc061a34193&format=json",
             method: 'GET'
         }).then(function (response) {
             console.log(response);
-            var artist = response.response.hits[0].result.primary_artist.api_path
-            console.log(artist);
-            $.ajax({
-                url: "https://api.genius.com" + artist + "/?access_token=A3QFPk4RZGxV8fgT_41uiJiCAeyXq-UhJ-xnxipOkgZSHnShtSRVdesaqTR8axQS",
-                method: 'GET'
-            }).then(function (reponse) {
-                console.log(reponse);
-                var bioPic = $("<div>");
-                var img = "<img>";
-                    img.attr("src", response);
-            })
+        })
+    }
 
 
- 
-})
+
 }
 
 function topArtist() {
@@ -91,18 +111,63 @@ function topArtist() {
         for (var i = 0; i < 10; i++) {
             var li = $("<li>");
             li.html(response.artists.artist[i].name);
+            li.addClass("list");
+            li.data("artist",response.artists.artist[i].name);
             ol.append(li);
         }
+        
+
     })
 }
-function topTrack() {
-    $.ajax({
-        url: "http://ws.audioscrobbler.com/2.0/?method=chart.gettoptracks&api_key=4eb509c03c98813c8a254fc061a34193&format=json",
-        method: 'GET'
-    }).then(function (response) {
-        console.log(response);
-    })
-}
+
+
 
 topArtist();
 
+    $(document).on("click", ".list", function () {
+        $("#youtube").empty();
+        $("#genius").empty();
+
+        var liLink = $(this).data("artist");
+    $.ajax({
+
+        url: "https://api.genius.com/search?q=" + liLink +
+            "&access_token=A3QFPk4RZGxV8fgT_41uiJiCAeyXq-UhJ-xnxipOkgZSHnShtSRVdesaqTR8axQS",
+        method: 'GET'
+    }).then(function (response) {
+
+        console.log(response);
+
+        var artist = response.response.hits[0].result.primary_artist.api_path
+
+        console.log(artist);
+
+        $.ajax({
+            url: "https://api.genius.com" + artist +
+                "/?access_token=A3QFPk4RZGxV8fgT_41uiJiCAeyXq-UhJ-xnxipOkgZSHnShtSRVdesaqTR8axQS" +
+                "&text_format=html",
+            method: 'GET'
+        }).then(function (response) {
+            var bioPic = $("<div>");
+            var img = $("<img>");
+            var x = response.response;
+            img.attr("src", x.artist.image_url);
+            bioPic.append(img);
+            $("#genius").prepend(bioPic);
+            console.log(x.artist.name);
+            console.log(x.artist.alternate_names);
+            var name = x.artist.alternate_names;
+            var moreName = $("<div>");
+            moreName.append(name);
+            $("#genius").append(moreName)
+
+            var describe = x.artist.description.html;
+            console.log(describe)
+            var bio = $("<div>");
+            bio.append(describe);
+            $("#genius").append(bio);
+
+        })
+    })  
+
+})
